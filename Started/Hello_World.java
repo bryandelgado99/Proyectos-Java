5a4e4281759c2ea63977a9c3265ec2624494e7af
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2023-05-23 08:58:31 -0500

    Elementos de inicio para estudio de POO

---
+++ Started/Hello_World.java
@@ -0,0 +1,11 @@
+/**
+ * Hello World Application
+ */
+
+public class Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+ 	System.out.println("Me llamo Bryan Delgado");
+	 System.out.println("Soy un Game Developer");
+    }
+}
