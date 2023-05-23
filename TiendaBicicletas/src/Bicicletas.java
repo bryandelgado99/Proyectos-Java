6191af3bd21f34da2978d7ed3dcf7cfff00c741b
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2023-05-23 08:57:19 -0500

    Proyecto Bicicleta para estudio de Objetcs

---
+++ TiendaBicicletas/src/Bicicletas.java
@@ -0,0 +1,20 @@
+public class Bicicletas {
+    String tipo;
+    int marchas;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getMarchas() {
+        return marchas;
+    }
+
+    public void setMarchas(int marchas) {
+        this.marchas = marchas;
+    }
+}
