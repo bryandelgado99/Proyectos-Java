4bfe1544ffe2a126839e87b18e4318d7b6d0c25b
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2023-07-04 08:12:13 -0500

    Ejercicio 13.2 con Manejo de Excepciones try y catch

---
+++ Excepciones/Excepciones/src/ejercicio_13_2.java
@@ -0,0 +1,5 @@
+public class ejercicio_13_2 {
+    public static int cociente( int numerador, int denominador ) throws ArithmeticException{
+        return numerador / denominador; // posible división entre cero
+    } // fin del método cociente
+}
