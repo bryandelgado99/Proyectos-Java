d0fb0e8dc56d0899440da87972542ee33857e096
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2023-07-04 07:20:30 -0500

    Manejo de Excepciones

---
+++ Excepciones/Excepciones/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        try{
+            int [] myNUmbers = {1,2,3};
+            System.out.println(myNUmbers[10]);
+        }
+        catch (Exception e){
+            System.out.println("Algo ha ido mal");
+        }
+    }
+}
